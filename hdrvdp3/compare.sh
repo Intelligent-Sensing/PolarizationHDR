1419d6b9a1d2c717fba20d9f7f7730ba8e42fb60
Matthew Chan <mattchan@cs.umd.edu>
2023-07-27 18:37:47 -0400

    Remove old project code

--- hdrvdp3/compare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. /usr/share/Modules/init/bash
-module add matlab/2019b
-matlab -nodisplay -r "cd('hdrvdp3');addpath('utils');cd('examples');compare('$1','$2', '$3');exit"
-# matlab -nodisplay -r "addpath('utils');cd('./examples');display_quality;exit"
